35ad4c68a920159e457faefdb78d54f3845e935c
Shuvo-Jyoti-Sarkar <shuvojyoti98@gmail.com>
2022-03-02 13:01:02 +0600

    new part added

--- js/index.js
+++ js/index.js
@@ -11,12 +11,14 @@ const searchPhone = () => {
         .then(data => displaySearchResult(data.data));
 }
 
+// Function to display search results
 const displaySearchResult = (phones) => {
     const searchResult = document.getElementById('search-result');
     for (const phone of phones) {
-        // console.log(phone);
+        // Creating new div with dynamic html elemnts
         const div = document.createElement('div');
         div.classList.add('col');
+        // Adding html elements inside the div
         div.innerHTML = `
         <div class="card h-25 w-75 mx-auto">
             <img src="${phone.image}" class="card-img-top" alt="...">
@@ -27,9 +29,12 @@ const displaySearchResult = (phones) => {
             <button class="btn btn-info" onclick="loadPhoneDetails('${phone.slug}')">Show Details</button>
         </div>    
         `;
+        // Adding new div inside parent div
         searchResult.appendChild(div);
     }
 }
+
+// Getting details of a single phone
 const loadPhoneDetails = phoneId => {
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
     fetch(url)
@@ -37,17 +42,23 @@ const loadPhoneDetails = phoneId => {
         .then(data => displayPhoneDetails(data.data));
 }
 
+// Function to display single phone details
 const displayPhoneDetails = phone => {
     console.log(phone);
     const phoneDetails = document.getElementById('phone-details');
+    // Creating div to show phone details
     const div = document.createElement('div');
     div.classList.add('card');
+    // Adding html elements to div
     div.innerHTML = `
             <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">${phone.name}</h5>
-                
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <h3 class="card-title">${phone.name}</h3>
+                <h4>${phone.releaseDate}</h4>
+                <h5><strong>Chipset:</strong> ${phone.mainFeatures.chipSet}</h5>
+                <h5><strong>Memory:</strong> ${phone.mainFeatures.memory}</h5>
+                <h5><strong>Storage:</strong> ${phone.mainFeatures.storage}</h5>
+                <h5><strong>Display Size:</strong> ${phone.mainFeatures.displaySize}</h5>
             </div>
     `;
     phoneDetails.appendChild(div);
